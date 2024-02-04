ad7b741547beff5b450b534e8b8f210ef5fdab12
buJJI7865 <158800792+buJJI7865@users.noreply.github.com>
2024-02-04 13:38:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,19 @@ menubar.onclick = () =>{
     mynav.classList.toggle('active');
 }
 
-            var form = document.getElementById('sheetdb-form');
-        form.addEventListener("submit", e => {
-          e.preventDefault();
-          fetch(form.action, {
-              method : "POST",
-              body: new FormData(document.getElementById("sheetdb-form")),
-          }).then(
-              response => response.json()
-          ).then((html) => {
-            // you can put any JS code here
-            window.open('https://www.vmrcarrental.com/thank-you', '_blank');
+// script.js
+var form = document.getElementById('sheetdb-form');
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    fetch(form.action, {
+        method: "POST",
+        body: new FormData(document.getElementById("sheetdb-form")),
+    }).then(
+        response => response.json()
+    ).then((html) => {
+        // you can put any JS code here
+        window.open('https://www.vmrcarrental.com/thank-you', '_blank');
+    });
+});
 
-          });
-        });
      
